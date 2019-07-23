52bfdbf251d7b9f10d0ca883ff0174babc3ee26b
Luca Molteni <volothamp@gmail.com>
2019-07-23 15:45:14 +0200

    Super KieBase

--- src/test/java/org/kie/kproject/IncludesTest.java
+++ src/test/java/org/kie/kproject/IncludesTest.java
@@ -22,13 +22,15 @@ public class IncludesTest {
 
     private final String SUPER_KBASE_NAME = "SuperKbase";
     private final String SUPER_KBASE_PACKAGE = "org.superkbase";
+    private final ReleaseIdImpl SUPER_RELEASE_ID = new ReleaseIdImpl(SUPER_KBASE_PACKAGE, "superkiebase", "1.0.0");
+
 
     @Test
     public void testChildKBase() {
         KieBase childKBase = createChildKBase();
 
         KieSession newChildKSession = childKBase.newKieSession();
-        FactHandle stringDeleted = newChildKSession.insert("tobedeleted");
+        FactHandle stringDeleted = newChildKSession.insert("10");
         FactHandle stringInserted = newChildKSession.insert("otherstring");
 
         int numberOfRulesFired = newChildKSession.fireAllRules();
@@ -39,14 +41,28 @@ public class IncludesTest {
 
     }
 
+    @Test
+    public void testSuperKieBase() {
+        KieBase superKieBase = createSuperKieBase();
+
+        KieSession newSuperKieBase = superKieBase.newKieSession();
+        newSuperKieBase.insert(10);
+
+        int numberOfRulesFired = newSuperKieBase.fireAllRules();
+        assertEquals(1, numberOfRulesFired);
+        assertEquals(2, newSuperKieBase.getObjects().size());
+    }
+
     private KieBase createChildKBase() {
         KieServices kieServices = KieServices.Factory.get();
         KieModuleModel kmodule = kieServices.newKieModuleModel();
         KieBaseModel childKbase = kmodule.newKieBaseModel(CHILD_KBASE_NAME).setDefault(true);
         childKbase.addPackage(CHILD_KBASE_PACKAGE);
+
         KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
         kieFileSystem.writeKModuleXML(kmodule.toXML());
         kieFileSystem.generateAndWritePomXML(CHILD_RELEASE_ID);
+
         InputStreamResource resource = new InputStreamResource(getClass().getClassLoader().getResourceAsStream("org/childkbase/childrules.drl"));
         kieFileSystem.write("src/main/resources/org/childkbase/childrules.drl", resource);
         kieServices.newKieBuilder(kieFileSystem).buildAll(ExecutableModelFlowProject.class);
@@ -56,11 +72,17 @@ public class IncludesTest {
     private KieBase createSuperKieBase() {
         KieServices kieServices = KieServices.Factory.get();
         KieModuleModel superKModule = kieServices.newKieModuleModel();
-        KieBaseModel superKieBase = superKModule.newKieBaseModel(SUPER_KBASE_NAME).addInclude(CHILD_KBASE_NAME);
+        KieBaseModel superKieBase = superKModule.newKieBaseModel(SUPER_KBASE_NAME)
+                .setDefault(true);
         superKieBase.addPackage(SUPER_KBASE_PACKAGE);
+
         KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
         kieFileSystem.writeKModuleXML(superKModule.toXML());
+        kieFileSystem.generateAndWritePomXML(SUPER_RELEASE_ID);
+
+        InputStreamResource resource = new InputStreamResource(getClass().getClassLoader().getResourceAsStream("org/superkiebase/superrules.drl"));
+        kieFileSystem.write("src/main/resources/org/superkiebase/superrules.drl", resource);
         kieServices.newKieBuilder(kieFileSystem).buildAll(ExecutableModelFlowProject.class);
-        return kieServices.newKieContainer(CHILD_RELEASE_ID).getKieBase();
+        return kieServices.newKieContainer(SUPER_RELEASE_ID).getKieBase();
     }
 }
